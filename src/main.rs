dc1a0543d489d71e174aa2a3b505a6d5edb59ec1
anhony1 <areyes5141@gmail.com>
2025-02-06 19:46:58 -0700

    huh

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod server;
 
+
 fn main() {
     
     println!("Hello, world!");
--- src/server.rs
+++ src/server.rs
@@ -2,51 +2,10 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
 use rand::Rng;
-use std::sync;
 
-struct Worker { 
+use lib::*;
 
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
-
-}
-
-impl Worker {
-    fn new(
-        id: usize,
-        job_queue: Arc<SegQueue<Job>>,
-        job_signal: Arc<(Mutex<bool>, Condvar)>,
-        running: Arc<AtomicBool>,
-    ) -> Worker {
-        let thread = thread::spawn(move || loop {
-            match job_queue.pop() {
-                Some(Job::Task(task)) => if let Err(_) = task() {},
-                Some(Job::Shutdown) => {
-                    break;
-                }
-                None => {
-                    let (lock, cvar) = &*job_signal;
-                    let mut job_available = lock.lock().unwrap();
-                    while !*job_available && running.load(Ordering::Relaxed) {
-                        job_available = cvar
-                            .wait_timeout(job_available, Duration::from_millis(100))
-                            .unwrap()
-                            .0;
-                    }
-                    *job_available = false;
-                }
-            }
-        });
-
-        Worker {
-            id,
-            thread: Some(thread),
-        }
-    }
-}
-
-
-fn handle_client(stream: TcpStream) {
+fn handle_client_threading(stream: TcpStream) {
 
     // Start a new thread for every time handle_client get's called. 
 
@@ -84,7 +43,7 @@ pub fn start() {
         match stream {
 
             Ok(s) => {
-                handle_client(s);
+                handle_client_threading(s);
             }
             Err(e) => {
                 eprintln!("Error Encountered: {}", e);
@@ -95,6 +54,46 @@ pub fn start() {
     }
 
 }
+
+
+pub fn start_server_pool() {
+
+    println!("Starting Server...");
+
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        
+        match stream {
+
+            Ok(s) => {
+                
+                pool.execute(|| {
+                    handle_connection(s);
+                });
+
+            }
+            Err(e) => {
+                eprintln!("Error Encountered: {}", e);
+            }
+ 
+        }
+
+    }
+
+}
+
+
+fn handle_connection(stream: TcpStream) -> i32 {
+
+
+    let do_something: i32 = 10;
+
+    do_something
+}
+
 
 
 pub fn stop() {
